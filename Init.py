f525a0bead8d187ac18860ce414ece4660638798
roei2106 <116170939+roei2106@users.noreply.github.com>
2022-12-05 11:04:01 +0200

    Update Init.py

--- Init.py
+++ Init.py
@@ -17,3 +17,29 @@ class Init:
             del self[key]
             return c
         return None
+    
+    import pickle
+from database import DataBase
+
+
+class BaseData(DataBase):
+    def __init__(self, file):
+        super().__init__(file)
+
+    def get_value(self, key):
+        file = open("roei.txt", "w")
+        self.dict = pickle.load(file)
+        file.close()
+        return super().get_value(key)
+
+    def set_value(self, key, val):
+        file = open("roei.txt", "w")
+        self.dict = pickle.load(file)
+        file.close()
+        super().get_value(key)
+
+    def delete_value(self, key):
+        self.dict = super().delete_value(key)
+        file = open("roei.txt", "w")
+        pickle.dump(dict)
+        file.close()
